28a73b0fd45f9f852a07bbc9d8402fe80c3be7db
Krunoslav Husak <kruno@binel.hr>
2021-01-23 12:14:55 +0100

    Added comments to example

--- example/main.go
+++ example/main.go
@@ -10,8 +10,20 @@ import (
 
 func main() {
 	s := goseismic.NewSeismic()
+
+	// Set Debug field to true if you want to log what's happening behind the scenes:
+	// connecting / disconnecting from websocket, sending ping and receiving pong etc.
+	// default is off (s.Debug = false)
 	s.Debug = true
+
+	// Set KeepAlive to true to keep connection alive by sending pings every minute.
+	// It will also disconnect and reconnect if some error ocurred while reading message.
+	// default is on (s.KeepAlive = true)
 	s.KeepAlive = true
+
+	// Connect connects to websocket and starts receiving messages when successfully connected.
+	// If KeepAlive is set to true and it fails to connect, it will try to automatically reconnect
+	// every 15sec, otherwise it will return error.
 	s.Connect()
 	defer s.Disconnect()
 
@@ -21,6 +33,7 @@ func main() {
 	for {
 		select {
 		case e := <-s.Events:
+			// process received Event here
 			log.Println(e)
 		case <-interrupt:
 			return
